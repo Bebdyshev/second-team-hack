d7794a6804e05860debab1ed3a0143bb3ef5dc48
Your Name <your@email.com>
2026-03-19 12:49:59 +0500

    favicon added

---
+++ frontend/app/icon.tsx
@@ -0,0 +1,23 @@
+import { ImageResponse } from 'next/og'
+
+export const size = { width: 32, height: 32 }
+export const contentType = 'image/png'
+
+const Icon = () =>
+  new ImageResponse(
+    <div
+      style={{
+        fontSize: 24,
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      🏢
+    </div>,
+    { ...size },
+  )
+
+export default Icon
